65db8608ad743ba68f4d646d935f4c6a067c801b
CanhotoFT <67978603+CanhotoFT@users.noreply.github.com>
2024-01-31 20:32:02 -0400

    create authenticator

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+
+COOKIE_EXPIRY_DAYS = 30
+
+def main():
+    authenticator = stauth.Authenticate(
+        {'usarnames': {'teste': {'name': 'testando', 'password': 'blabla'}}},
+        'random_cookie_name',
+        'random_signature_key',
+
+        COOKIE_EXPIRY_DAYS,
+    )
+
+def login_form(authenticator):
+    name, authenticator_status, username = authenticator.login('Login')
+    if authenticator_status:
+        authenticator.logout('Logout', main)
+        st.title('Area do Dasboard')
+
+if __name__ == '__main__':
+    main()
